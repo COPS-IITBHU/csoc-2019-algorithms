c757cdf9f445469ac63f6f383fcd05ac7cc44bac
KRT2305 <44458869+KRT2305@users.noreply.github.com>
2019-10-28 15:24:42 +0530

    Create pigeonhole sort.py

---
+++ Python/pigeonhole sort.py
@@ -0,0 +1,23 @@
+def pigeonhole_sort(a): 
+    # size of range of values in the list  
+    # (ie, number of pigeonholes we need) 
+    my_min = min(a) 
+    my_max = max(a) 
+    size = my_max - my_min + 1
+  
+    # our list of pigeonholes 
+    holes = [0] * size 
+  
+    # Populate the pigeonholes. 
+    for x in a: 
+        assert type(x) is int, "integers only please"
+        holes[x - my_min] += 1
+  
+    # Put the elements back into the array in order. 
+    i = 0
+    for count in range(size): 
+        while holes[count] > 0: 
+            holes[count] -= 1
+            a[i] = count + my_min 
+            i += 1
+              
